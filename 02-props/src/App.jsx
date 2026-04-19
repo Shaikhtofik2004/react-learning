20c9172534c296602d83871c507503d0ce47e7e3
sameerEexpansionjs <sameer@expansionjs.com>
2026-04-19 12:06:53 +0530

    Added 02-props concept

---
+++ 02-props/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from './components/Card';
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Card
+        user="Atif Khan"
+        age={21}
+        img="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <Card
+        user="Hasim Zoya"
+        age={25}
+        img="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <Card
+        user="Mohammed Hamza"
+        age={26}
+        img="https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </div>
+  );
+};
+
+export default App;
